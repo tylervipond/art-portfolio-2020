6214f749338578d8e668f85975caa903c09c00e3
Tyler Vipond <tylervipond@gmail.com>
2019-11-29 14:03:28 -0500

    netlify added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Art by Tyler Vipond`,
+    description: `A Portfolio of Artworks by Toronto based artist Tyler Vipond.`,
+    author: `Tyler Vipond`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -27,6 +30,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.
+      },
+    },
+    `gatsby-plugin-netlify`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
