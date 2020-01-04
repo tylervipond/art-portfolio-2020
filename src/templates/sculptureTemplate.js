33443b42b6a2c949ca1513a8fffe86b4e69855ec
Tyler Vipond <tylervipond@gmail.com>
2020-01-03 22:55:56 -0500

    temporarily disable some queries

--- src/templates/sculptureTemplate.js
+++ src/templates/sculptureTemplate.js
@@ -14,17 +14,17 @@ export default function Template({data}) {
   );
 }
 
-export const pageQuery = graphql`
-  query($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
-      frontmatter {
-        title
-        year
-        size
-        media
-        image
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query($path: String!) {
+//     markdownRemark(frontmatter: { path: { eq: $path } }) {
+//       html
+//       frontmatter {
+//         title
+//         year
+//         size
+//         media
+//         image
+//       }
+//     }
+//   }
+// `
