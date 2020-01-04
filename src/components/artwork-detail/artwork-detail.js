f005d47c124968a7565b9951dff60b51ebfe48f4
Tyler Vipond <tylervipond@gmail.com>
2020-01-04 15:43:59 -0500

    add uploadcare media library

--- src/components/artwork-detail/artwork-detail.js
+++ src/components/artwork-detail/artwork-detail.js
@@ -7,7 +7,7 @@ import { COLOR_TEXT } from '../../styles/colors';
 export const ArtworkDetail = styled.p`
 font-size: ${FONT_SIZE_2};
 font-family: ${TYPE_FACE_COPY};
-font-style: emphasis;
+font-style: italic;
 color: ${COLOR_TEXT};
 margin: 0;
 `
