258861dc81ca2b69dc040753d1823c2f835acbd9
Tyler Vipond <tylervipond@MacBook-Pro.local>
2020-08-08 23:22:56 -0400

    updated link focus colors

--- src/components/link.js
+++ src/components/link.js
@@ -1,19 +1,20 @@
 import styled from '@emotion/styled';
 import { Link as GatsbyLink } from 'gatsby';
 import zigzag from '!file-loader!../../assets/images/zigzag.svg';
-import { SPACE_SMALL, asRem } from '../styles';
+import { SPACE_SMALL, asRem, COLOR_TEXT_HOVER, COLOR_BLACK } from '../styles';
 
 export const Link = styled(GatsbyLink)`
   color: inherit;
   text-decoration: none;
   position: relative;
   transition: color 0.5s ease;
+  outline: none;
   &:visited {
     color: inherit;
   }
   &:hover,
   &:focus {
-    color: darkred;
+    color: ${COLOR_TEXT_HOVER};
   }
   &::after {
     content: '';
@@ -26,8 +27,19 @@ export const Link = styled(GatsbyLink)`
     opacity: 0;
     transition: opacity 0.3s ease;
   }
-  &:hover::after,
-  &:focus::after {
+  &:hover::after {
     opacity: 1;
+  }
+  &::before {
+    position: absolute;
+    content: '';
+    left: -1px;
+    right: -1px;
+    top: -1px;
+    bottom: -1px;
+    border: 1px solid transparent;
+  }
+  &:focus::before {
+    border-color: ${COLOR_BLACK};
   }
 `;
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,1 +1,4 @@
-export const COLOR_TEXT = 'hsla(0, 5%, 5%, 1)';
+export const COLOR_BLACK = 'hsla(0, 5%, 5%, 1)';
+export const COLOR_DARK_RED = 'hsla(0, 100%, 27%, 1.0)';
+export const COLOR_TEXT = COLOR_BLACK;
+export const COLOR_TEXT_HOVER = COLOR_DARK_RED;
